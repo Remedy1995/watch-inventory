1d1fa0daee72554b717c4404b60b167641a05e01
Remedy1995 <adjetadjetey45@gmail.com>
2023-05-07 13:41:51 +0000

    made a small change

--- middleware/Role.js
+++ middleware/Role.js
@@ -2,6 +2,7 @@ const RoleAuth = (req, res, next) => {
     if (req.cookies.role === "user" && req.session.username) {
         return res.redirect('/userhome');
     }
+    //move this to the admin dashboard
      if (req.cookies.role === "admin" && req.session.username) {
         return res.redirect('/adminhome');
     }
